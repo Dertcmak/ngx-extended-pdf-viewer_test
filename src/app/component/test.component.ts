d361e3d83be4856a4b3c83989d49a580f9de07d3
dertcaf <dertcaf@oksystem.cz>
2018-12-04 13:32:14 +0100

    solving the issue https://github.com/stephanrauh/ngx-extended-pdf-viewer/issues/12

--- src/app/component/test.component.ts
+++ src/app/component/test.component.ts
@@ -1,22 +1,36 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-test-component',
   templateUrl: './test.component.html'
 })
-export class TestComponent implements OnInit {
+export class TestComponent implements OnInit, OnDestroy {
 
   pdfURL = 'assets/pdf.pdf';
 
   constructor(private router: Router) {
   }
 
+  private pdfViewerCleanUp() {
+    if ((window as any).PDFViewerApplication) {
+      (window as any).PDFViewerApplication.unbindEvents();
+      (window as any).PDFViewerApplication.unbindWindowEvents();
+      (window as any).PDFViewerApplication.cleanup();
+      (window as any).PDFViewerApplication.close();
+    }
+  }
+
   back() {
+    this.pdfViewerCleanUp();
     this.router.navigateByUrl('dashboard');
   }
 
   ngOnInit(): void {
     console.log('Initialized');
+  }
+
+  ngOnDestroy(): void {
+    this.pdfViewerCleanUp();
   }
 }
